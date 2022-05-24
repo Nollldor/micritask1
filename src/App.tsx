2da2aeecef84881c767c32643d8b3fb239eeb877
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-24 13:56:40 +0300

    microtask2

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import './App.css';
 import {Header} from "./Header";
 import {Body} from "./Body";
 import {Footer} from "./Footer";
+import {NewComponent} from "./NewComponent";
 
 function App() {
+    const students = [
+        {id:0, name: "Arthur", age:30},
+        {id:1, name: "Kristina", age:29}
+    ]
   return (
     <>
-        <Header title={"NEW Header"}/>
-        <Body titleForBody={"New Body"}/>
-        <Footer titleForFooter={"New Footer"}/>
+
+        {/*<Header title={"NEW Header"}/>
+        <Body titleForBody={"New Body"}/>*/}
+        <NewComponent students={students} />
+        {/*<Footer titleForFooter={"New Footer"}/>*/}
     </>
   );
 }
