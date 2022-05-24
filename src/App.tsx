c0c8524edb576849879aba02625ea066bb104c4c
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-24 12:28:02 +0300

    microtask1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Header";
+import {Body} from "./Body";
+import {Footer} from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+    <>
+        <Header title={"NEW Header"}/>
+        <Body titleForBody={"New Body"}/>
+        <Footer titleForFooter={"New Footer"}/>
+    </>
   );
 }
 
